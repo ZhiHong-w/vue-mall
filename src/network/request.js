225811b0ba153039a8cc29db0933b44e972bbb05
ZhiHong-w <18656872215@163.com>
2020-06-07 21:09:02 +0800

    'add'

---
+++ src/network/request.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export function request(config) {
+    const instance=axios.create({
+      baseURL:'http://152.136.185.210:8000/api/n3',
+      timeout:5000
+    })
+
+  instance.interceptors.request.use(config=>{
+    return config
+  },error => {
+
+  })
+
+  instance.interceptors.response.use(res=>{
+    return res.data
+  },err=>{
+
+  })
+    return instance(config);//返回一个promise所以可以用then
+}
+
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,38 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
+  import {getHomeMultidata} from "../../network/home";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components:{
+      NavBar,
+    },
+    data(){
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res=>{
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
 </style>
