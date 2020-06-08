e226081cafc1af9ddd73e4ddc58ac090eed20bd0
ZhiHong-w <18656872215@163.com>
2020-06-08 09:55:53 +0800

    'add'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,22 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
   import {getHomeMultidata} from "../../network/home";
-
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
   export default {
     name: "Home",
     components:{
       NavBar,
+      HomeSwiper,
+      RecommendView,
     },
     data(){
       return {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props:{
+      recommends:{
+        props: Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+ .recommend{
+   display: flex;
+   width: 100%;
+   text-align: center;
+   font-size: 16px;
+   padding-bottom: 25px;
+   padding-top: 15px;
+   border-bottom: 8px solid #eeeeee;
+ }
+ .item{
+   flex: 1;
+ }
+ .item img{
+    width: 80px;
+    height: 80px;
+   margin-bottom: 5px;
+ }
+
+</style>
